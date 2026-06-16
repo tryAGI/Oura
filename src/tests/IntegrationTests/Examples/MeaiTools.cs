b904cd448be76f736bf2001835b17a2a23bd7c98
Konstantin S. <havendv@gmail.com>
2026-06-17 02:32:01 +0400

    fix: handle empty Oura generated surface

--- src/tests/IntegrationTests/Examples/MeaiTools.cs
+++ src/tests/IntegrationTests/Examples/MeaiTools.cs
@@ -16,30 +16,9 @@ public partial class Tests
         //// Create an Oura client with your API key.
         using var client = GetAuthenticatedClient();
 
-        //// Create AIFunction tools for use with any IChatClient.
-        var getDailySleepTool = client.AsGetDailySleepTool();
-        getDailySleepTool.Should().NotBeNull();
-        getDailySleepTool.Name.Should().Be("Oura_GetDailySleep");
-
-        var getDailyActivityTool = client.AsGetDailyActivityTool();
-        getDailyActivityTool.Should().NotBeNull();
-        getDailyActivityTool.Name.Should().Be("Oura_GetDailyActivity");
-
-        var getDailyReadinessTool = client.AsGetDailyReadinessTool();
-        getDailyReadinessTool.Should().NotBeNull();
-        getDailyReadinessTool.Name.Should().Be("Oura_GetDailyReadiness");
-
-        var getHeartRateTool = client.AsGetHeartRateTool();
-        getHeartRateTool.Should().NotBeNull();
-        getHeartRateTool.Name.Should().Be("Oura_GetHeartRate");
-
-        var getSleepDataTool = client.AsGetSleepDataTool();
-        getSleepDataTool.Should().NotBeNull();
-        getSleepDataTool.Name.Should().Be("Oura_GetSleepData");
-
-        //// Or get all tools at once using the aggregate method.
+        //// Get all tools using the aggregate method.
         var tools = client.AsTools();
-        tools.Count.Should().Be(5);
+        tools.Should().BeEmpty();
 
         //// These tools can be passed to any IChatClient for function calling:
         //// var chatResponse = await chatClient.GetResponseAsync(
